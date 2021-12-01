4808985d52513cff6ffec2c6c6d80892edf20456
Jake Smolka <jake.smolka@better.care>
2021-12-01 18:41:41 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,31 @@
 fun main() {
+    fun countComparison(numbers: List<Int>): Int {
+        val iterator = numbers.listIterator()
+
+        var counter = 0
+        while (iterator.hasNext()) {
+            if (iterator.hasPrevious() &&
+                (numbers[iterator.previousIndex()] < numbers[iterator.nextIndex()]))
+                counter++
+            iterator.next()
+        }
+
+        return counter
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val numbers = input.map { it.toInt() }
+
+        return countComparison(numbers)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val numbers = input.map { it.toInt() }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        val windows = numbers.windowed(3) { it.sum() }
+
+        return countComparison(windows)
+    }
 
     val input = readInput("Day01")
     println(part1(input))
